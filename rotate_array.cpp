56ab17d177323fba0de880087575d61a8a8b9a6b
basant-kumar <basantkmeena@gmail.com>
2017-08-04 19:48:18 +0530

    added rotate array using efficient method in O(n) time and O(1) space

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -2,34 +2,55 @@
 #include <vector>
 using namespace std;
 
+int gcd(int a,int b)
+{
+   if(b==0)
+     return a;
+   else
+     return gcd(b, a%b);
+}
+
 void rotate(vector<int> &arr,int n,int d){
     /*  time complexity O(n)
         spcae complexity O(1)
     */
-    int first=a[0], i=0, index;
-    while(1) {
-        index = (i+d)%n;
-        if(!index) {
-            a[i] = first;
-            break;
-        }
-        a[i] = a[index];
-        i = index;
+    int i, j, k, temp;
+    for (i = 0; i < gcd(d, n); i++)
+    {
+    /* move i-th values of blocks */
+    temp = arr[i];
+    j = i;
+    while(1)
+    {
+      k = j + d;
+      if (k >= n)
+        k = k - n;
+      if (k == i)
+        break;
+      arr[j] = arr[k];
+      j = k;
+    }
+    arr[j] = temp;
     }
     
     
 }
 
 int main() {
-    int n,d;
-    cin>>n>>d;
-    vector<int> arr(n);
-    for(int i=0;i<n;++i){
-        cin>>arr[i];
+    int t;
+    cin>>t;
+    while(t--){
+        int n,d;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;++i){
+            cin>>arr[i];
+        }
+        cin>>d;
+        rotate(arr,n,d);
+        for(int i=0;i<n;++i){
+            cout<<arr[i]<<" ";
+        }cout<<endl;
     }
-    rotate(arr,n,d);
-    for(int i=0;i<n;++i){
-        cout<<arr[i]<<" ";
-    }cout<<endl;
 	return 0;
 }
