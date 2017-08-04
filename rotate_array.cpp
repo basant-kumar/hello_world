618705f1ff8f46bf41235a8f11a3b056e7ecf83f
basant-kumar <basantkmeena@gmail.com>
2017-08-04 19:35:39 +0530

    added rotate array using efficient method in O(n) time and (1) space

---
+++ rotate_array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate(vector<int> &arr,int n,int d){
+   int first=a[0], i=0, index;
+    while(1) {
+        index = (i+d)%n;
+        if(!index) {
+            a[i] = first;
+            break;
+        }
+        a[i] = a[index];
+        i = index;
+    }
+    
+    
+}
+
+int main() {
+    int n,d;
+    cin>>n>>d;
+    vector<int> arr(n);
+    for(int i=0;i<n;++i){
+        cin>>arr[i];
+    }
+    rotate(arr,n,d);
+    for(int i=0;i<n;++i){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+	return 0;
+}
